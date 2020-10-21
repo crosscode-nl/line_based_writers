57d21613084e4e508efd1560fa44a29ae8b9aa4f
Patrick Vollebregt <patrick@crosscode.nl>
2020-10-21 13:19:44 +0200

    Intitial development of line based writeres

--- include/line_based_writers.h
+++ include/line_based_writers.h
@@ -3,4 +3,55 @@
 
 #include "line_based_writers/version.h"
 
+namespace crosscode::line_based_writers {
+
+    template<typename Tstream_sink>
+    class stream_writer {
+    public:
+        using sink_type = Tstream_sink;
+    private:
+        sink_type sink_;
+    public:
+        template <typename ...Args>
+        explicit stream_writer(Args&&... args) : sink_{std::forward<Args>(args)...} {}
+        stream_writer() = default;
+        stream_writer(stream_writer<sink_type>&&) noexcept = default;
+        stream_writer(const stream_writer<sink_type>&) noexcept = delete;
+        stream_writer<sink_type>&operator=(const stream_writer<sink_type>&) = delete;
+
+        template<typename Tline>
+        void operator()(Tline &&line) {
+            sink_ << std::forward<Tline>(line) << "\n";
+        }
+
+        sink_type& sink() { return sink_; }
+    };
+
+
+    template<typename Tline_based_protocol_sink>
+    class line_buffer {
+    public:
+        using sink_type = Tline_based_protocol_sink;
+    private:
+        std::size_t buffer_size_;
+        sink_type sink_;
+    public:
+        template <typename Tbuffer_size, typename ...Args>
+        explicit line_buffer(Tbuffer_size buffer_size, Args&&... args) : buffer_size_{buffer_size}, sink_{std::forward<Args>(args)...} {}
+        template <typename Tbuffer_size>
+        explicit line_buffer(Tbuffer_size buffer_size) : buffer_size_{buffer_size} {}
+        line_buffer(line_buffer<sink_type>&&) noexcept = default;
+        line_buffer(const line_buffer<sink_type>&) noexcept = delete;
+        line_buffer<sink_type>&operator=(const line_buffer<sink_type>&) = delete;
+
+        template<typename Tline>
+        void operator()(Tline &&line) {
+            sink_(std::forward<Tline>(line));
+        }
+
+        sink_type& sink() { return sink_; }
+    };
+
+}
+
 #endif //LINE_BASED_WRITERS_LINE_BASED_WRITERS_H
--- tests/line_based_writers_tests.cpp
+++ tests/line_based_writers_tests.cpp
@@ -1,3 +1,39 @@
 #include "doctest.h"
 #include "line_based_writers.h"
+#include <sstream>
 
+namespace lbw = crosscode::line_based_writers;
+using namespace std::literals;
+
+using stringstream_stream_writer = lbw::stream_writer<std::stringstream>;
+
+TEST_SUITE("Line based writers") {
+    TEST_CASE("Can create stream writer with header") {
+        stringstream_stream_writer sw("some header\n",std::ios_base::app | std::ios_base::out);
+        SUBCASE("Can write line to stream_writer") {
+            sw("This is a line");
+            REQUIRE("some header\nThis is a line\n"==sw.sink().str());
+        }
+    }
+    TEST_CASE("Can create stream writer") {
+        stringstream_stream_writer sw;
+        SUBCASE("Can write line to stream_writer") {
+            sw("This is a line");
+            REQUIRE("This is a line\n"==sw.sink().str());
+        }
+    }
+    TEST_CASE("Can create line buffer of 1 with initial content") {
+        lbw::line_buffer<stringstream_stream_writer> lbssw{1u, "some header\n",std::ios_base::app | std::ios_base::out};
+        SUBCASE("Can write line to stream_writer") {
+            lbssw("This is a line");
+            REQUIRE("some header\nThis is a line\n"==lbssw.sink().sink().str());
+        }
+    }
+    TEST_CASE("Can create line buffer of 1") {
+        lbw::line_buffer<stringstream_stream_writer> lb{1u};
+        SUBCASE("Can write line to line buffer") {
+           lb("This is a line");
+           REQUIRE("This is a line\n"==lb.sink().sink().str());
+        }
+    }
+}
